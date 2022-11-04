e958578ea284baef0d676ca37eac7c84b6dcd91a
Simone Giampà <simox1997@hotmail.it>
2022-11-04 08:17:09 +0100

    works

--- src/main/java/com/example/spikejunitmockito/BLL/DTO/BookDTO.java
+++ src/main/java/com/example/spikejunitmockito/BLL/DTO/BookDTO.java
@@ -1,14 +1,19 @@
 package com.example.spikejunitmockito.BLL.DTO;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 
 @Data
+//Serve per mockito
+@EqualsAndHashCode
 public class BookDTO {
 
     private Integer id;
     private String name;
     private String summary;
     private Integer rating;
+
 
 
 }
--- src/test/java/com/example/spikejunitmockito/BookControllerTest.java
+++ src/test/java/com/example/spikejunitmockito/BookControllerTest.java
@@ -2,30 +2,29 @@ package com.example.spikejunitmockito;
 
 import com.example.spikejunitmockito.BLL.DTO.BookDTO;
 import com.example.spikejunitmockito.BLL.service.impl.BookService;
-import com.example.spikejunitmockito.PL.controller.impl.BookController;
-import com.example.spikejunitmockito.PL.mapper.impls.BookRestMapper;
-import com.example.spikejunitmockito.PL.rest.BookRest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.Matchers.hasSize;
+import java.util.ArrayList;
+import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
-@WebMvcTest(BookController.class)
+//@WebMvcTest(BookController.class)
 public class BookControllerTest {
 
     @MockBean
@@ -35,35 +34,60 @@ public class BookControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private BookRestMapper mapper;
 
     @Autowired
     private ObjectMapper objectMapper;
 
 
-    //Unit test
+    //Unit test per la post
     @Test()
     public void shouldInsertBook() throws Exception {
 
-        BookRest atomicHabits = new BookRest(1, "Atomic Habits", "Learn how to develop good habits", 5);
+        BookDTO dto = new BookDTO();
+        dto.setId(1);
+        dto.setName("Atomic Habits");
+        dto.setSummary("Learn how to develop good Habits");
+        dto.setRating(5);
 
-        //Non posso fare il mapping direttamente nel then return sennò scoppia
-        BookDTO dto = mapper.fromRestToDto(atomicHabits);
         when(service.insert(any(BookDTO.class))).thenReturn(dto);
 
 
         mockMvc.perform(post("/book")
-
                         .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(atomicHabits)))
+                        .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$",hasSize(1)));
-
+                .andExpect(jsonPath("$.name").value(dto.getName()))
+                .andExpect(jsonPath("$.rating").value(dto.getRating()))
+                .andExpect(jsonPath("$.summary").value(dto.getSummary()))
+                .andExpect(jsonPath("$.id").value(dto.getId()));
 
     }
 
+    //Unit test per la get
+    @Test()
+    public void shouldGetBook() throws Exception{
+        BookDTO dto = new BookDTO();
+        dto.setId(1);
+        dto.setName("Atomic Habits");
+        dto.setSummary("Learn how to develop good Habits");
+        dto.setRating(5);
 
-    //Integration Test
+        List<BookDTO> result = new ArrayList<>();
+        result.add(dto);
+
+        when(service.getAll()).thenReturn(result);
+
+        mockMvc.perform(get("/book"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value(dto.getName()))
+                .andExpect(jsonPath("$[0].rating").value(dto.getRating()))
+                .andExpect(jsonPath("$[0].summary").value(dto.getSummary()))
+                .andExpect(jsonPath("$[0].id").value(dto.getId()));
+    }
+
+
+
+
+
 
 }
